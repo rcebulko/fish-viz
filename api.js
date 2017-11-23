8acc777c316b2df05f02cdd3e6c4e0d2fa46e81b
Ryan Cebulko <ryan@cebulko.com>
2017-11-23 16:55:02 -0500

    add limit parameter to API router

--- api.js
+++ api.js
@@ -8,7 +8,22 @@ var express = require('express'),
 
     PORT = 90;
 
-app.use('/api', seqRouter(schema.Species));
+app.use('/api', seqRouter(schema.Species, {
+    find: (req, res) => {
+        var limit = req.query.limit;
+        delete req.query.limit;
+
+        schema.Species.findAll({
+            where: req.query,
+            limit: limit
+        }).then(dbModel => {
+            res.json(dbModel);
+        }).catch(err => {
+            res.json(err);
+        });
+    }
+}));
+// app.use('/api', seqRouter(schema.Sample));
 
 http.createServer(app).listen(PORT, function () {
     console.log('Express server listening on port ' + PORT);
