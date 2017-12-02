e340153ed9e878be68a3f339ffec84f162f97075
Ryan Cebulko <ryan@cebulko.com>
2017-12-02 17:54:12 -0500

    API module

--- public/js/api.js
+++ public/js/api.js
@@ -1,78 +1,25 @@
-// Provides interface into Species/Sample API
-// must be loaded after `jQuery`
+// Dependencies:
+// - jQuery
+
 (function (exports) {
-    var API_PATH = 'http://localhost:1337/api/',
-        currentFilters = {};
-
-    // filter settings
-    function getRange(field) {
-        var range = currentFilters[field];
-        return [range.gt, range.lt];
-    }
-    function setBetweenFilter(field, min, max) {
-        currentFilters[field] = { gt: min, lt: max };
-    }
-
-    function getGeoBounds() {
-        return { lat: getRange('latitude'), lon: getRange('longitude') };
-    }
-    function setGeoBounds(latMin, latMax, lonMin, lonMax) {
-        setBetweenFilter('latitude', latMin, latMax);
-        setBetweenFilter('longitude', lonMin, lonMax);
-    }
-
-    function getDateRange() { return getRange('date'); }
-    function setDateRange(dateMin, dateMax) {
-        setBetweenFilter('date', dateMin, dateMax);
-    }
-
-    function getRegion() { return currentFilters.region; }
-    function setRegion(region) {
-        currentFilters.region = region;
-    }
-
+    var API_PATH = 'http://localhost:1337/api/';
 
     // fetch taxonomy data
-    function fetchSpeciesData(callback) {
-        $.get(API_PATH + 'species', callback);
+    function fetchSpecies() {
+        return $.get(API_PATH + 'species');
     }
 
-
     // fetch samples with arbitrary filters
-    function fetchSampleData(filterOpts, callback) {
-        $.get(API_PATH + 'sample',
-            Object.assign({ limit: 100 }, currentFilters, filterOpts),
-            callback
+    function fetchSamples(filterOpts) {
+        return $.get(API_PATH + 'sample',
+            Object.assign({ limit: 100 }, filterOpts),
         );
     }
 
-
-    // common use case queries
-    function fetchSpeciesSamples(speciesCode, callback) {
-        fetchSampleData({ species_code: speciesCode }, callback);
-    }
-    function fetchGenusSamples(genus, callback) {
-        fetchSampleData({ species: { genus: genus } }, callback);
-    }
-    function fetchFamilySamples(family, callback) {
-        fetchSampleData({ species: { family: family } }, callback);
+    function fetchSpeciesSamples(speciesCode, filterOpts, callback) {
+        return fetchSamples(
+            Object.assign({ species_code: speciesCode }, filterOpts));
     }
 
-
-    exports.API = {
-        getGeoBounds,
-        getDateRange,
-        getRegion,
-
-        setGeoBounds,
-        setDateRange,
-        setRegion,
-
-        fetchSpeciesData,
-        fetchSampleData,
-
-        fetchSpeciesSamples,
-        fetchGenusSamples,
-        fetchFamilySamples,
-    }
-}(window));
+    Object.assign(exports, { fetchSpecies, fetchSamples, fetchSpeciesSamples });
+}(window.API = window.API || {}));
