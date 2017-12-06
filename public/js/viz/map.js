5687188ffc953b9480b02c9dd3e5507dbee7c881
Ryan Cebulko <ryan@cebulko.com>
2017-12-06 13:58:03 -0500

    dynamic sample merging

--- public/js/viz/map.js
+++ public/js/viz/map.js
@@ -18,25 +18,64 @@
     mapLoaded = null,
     googlePromise = new Promise((resolve, reject) => { mapLoaded = resolve; }),
 
+    month = 1000 * 60 * 60 * 24 * 30,
+    fmtDates = dateRange => {
+        if (dateRange[1] - dateRange[0] < month) {
+            return Controls.DateRange.format(dateRange[0]);
+        } else {
+            return dateRange.map(Controls.DateRange.format)
+                .join(' to ');
+        }
+    },
+
+    fmtProtected = prot => {
+        if (prot > 0.99) {
+            return 'Yes';
+        } else if (prot < 0.01) {
+            return 'No';
+        } else {
+            return prot.toFixed(3) * 100 + '%';
+        }
+    },
+
     tip = d3.tip().attr('class', 'd3-tip')
         .direction('n')
         .html(sample => {
-            return [
-                '<b>Sample</b>',
-                [
-                    ['Date', Controls.DateRange.format(sample.date)],
-                    ['Latitude', sample.latitude.toFixed(5)],
-                    ['Longitude', sample.longitude.toFixed(5)],
-                    ['Depth', sample.depth],
-                    ['Length', sample.length],
-                    ['Number', sample.number],
-                    ['Protected', sample.protected ? 'Yes' : 'No'],
-                ].map(lbl_val => '<b>' + lbl_val.join('</b>: ')).join('<br>'),
-                sample.species.html(),
-            ].join('<br><br>')
+            if (sample.aggregated) {
+                return [
+                    '<b>' + sample.count + ' Samples</b>',
+                    [
+                        ['Date', fmtDates(sample.date)],
+                        ['Latitude', '~' + sample.latitude.toFixed(5)],
+                        ['Longitude', '~' + sample.longitude.toFixed(5)],
+                        ['Depth', sample.depth.map(d => d.toFixed(0)).join(' - ')],
+                        ['Length', sample.length.map(len => len.toFixed(2)).join(' - ')],
+                        ['Avg. Length', sample.avgLength.toFixed(2)],
+                        ['Number', sample.number.map(n => n.toFixed(1)).join(' - ')],
+                        ['Avg. Number', sample.avgNumber.toFixed(1)],
+                        ['Total Number', sample.totalNumber.toFixed(0)],
+                        ['Protected', fmtProtected(sample.protected)],
+                    ].map(lbl_val => '<b>' + lbl_val.join('</b>: ')).join('<br>'),
+                    sample.species.html(),
+                ].join('<br><br>');
+            } else {
+                return [
+                    '<b>Sample</b>',
+                    [
+                        ['Date', Controls.DateRange.format(sample.date)],
+                        ['Latitude', sample.latitude.toFixed(5)],
+                        ['Longitude', sample.longitude.toFixed(5)],
+                        ['Depth', sample.depth.toFixed(0)],
+                        ['Length', sample.length.toFixed(2)],
+                        ['Number', sample.number.toFixed(1)],
+                        ['Protected', sample.protected ? 'Yes' : 'No'],
+                    ].map(lbl_val => '<b>' + lbl_val.join('</b>: ')).join('<br>'),
+                    sample.species.html(),
+                ].join('<br><br>');
+            }
         }),
 
-    map, bounds, overlay;
+    map, mapNode,bounds, overlay;
 
 
     function init() {
@@ -46,7 +85,9 @@
             .then(() => {
                 console.info('Initializing map visualization');
 
-                map = new google.maps.Map(d3.select('.map-view').node(), {
+
+                mapNode = d3.select('.map-view').node();
+                map = new google.maps.Map(mapNode, {
                     zoom: 9,
                     mapTypeId: 'terrain',
                     disableDefaultUI: true,
@@ -64,11 +105,8 @@
                 Controls.Region.onChanged(fitRegion);
                 Samples.onChanged(drawSamples);
                 TaxonomyTree.onFocused(restyle);
-            })
-            .then(draw)
-            .then(() => {
-                initHistory();
-            });
+            }).then(draw)
+                .then(initHistory);
     }
 
 
@@ -142,7 +180,7 @@
 
     function initZoom() {
         google.maps.event.addListener(map, 'bounds_changed',
-            () => overlay.draw());
+            debounce(() => draw(), 250));
     }
 
 
@@ -150,9 +188,137 @@
         return Samples.getSamples().then(drawSamples);
     }
 
+    function nearBounds() {
+        var bounds = map.getBounds(),
+
+            ne = bounds.getNorthEast(),
+            sw = bounds.getSouthWest(),
+
+            latMin = sw.lat(),
+            lngMin = sw.lng(),
+            latMax = ne.lat(),
+            lngMax = ne.lng(),
+
+            latRange = latMax - latMin,
+            lngRange = lngMax - lngMin;
+
+        bounds.extend({ lat: latMin - latRange, lng: lngMin - lngRange });
+        bounds.extend({ lat: latMax + latRange, lng: lngMax + lngRange });
+        return bounds;
+    }
+
+
+    // sample pruning and merging
+    function geoPruneSamples(samples) {
+        var viewBounds = nearBounds(),
+            edges = viewBounds.toJSON(),
+
+            height = edges.north - edges.south,
+            width = edges.east - edges.west,
+
+            pxPerBucket = 20,
+
+            vertRes = 3 * mapNode.offsetHeight / pxPerBucket,
+            horizRes = 3 * mapNode.offsetWidth / pxPerBucket,
+
+            buckets = {},
+            getBucket = (val, min, max, res) =>
+                Math.floor(res * (val - min) / (max - min)),
+            getVertBucket = val =>
+                getBucket(val, edges.south, edges.north, vertRes),
+            getHorizBucket = val =>
+                getBucket(val, edges.west, edges.east, horizRes),
+            bucketKey = (vert, horiz) => vert + '__' + horiz,
+            sampleKey = sample => bucketKey(
+                getVertBucket(sample.latitude),
+                getHorizBucket(sample.longitude)),
+            i, key;
+
+        console.log('Starting with %d samples', samples.length);
+
+        samples = samples.filter(s =>
+                viewBounds.contains({ lat: s.latitude, lng: s.longitude }))
+
+        for (i = 0; i < samples.length; ++i) {
+            key = sampleKey(samples[i]);
+            if (typeof buckets[key] === 'undefined') buckets[key] = [];
+            buckets[key].push(samples[i]);
+        }
+
+        return Object.values(buckets)
+            .map(mergeSampleBucket)
+            .reduce((acc, arr) => acc.concat(arr), []);
+    }
+
+    function mergeSampleBucket(bucket) {
+        var samples = {},
+            i, sample;
+
+        for (i = 0; i < bucket.length; ++i) {
+            sample = bucket[i]
+            if (typeof samples[sample.species_code] === 'undefined') {
+                samples[sample.species_code] = sample;
+            } else {
+                samples[sample.species_code] = mergeSamples(
+                    samples[sample.species_code],
+                    sample);
+            }
+        }
+
+        return Object.values(samples);
+    }
+
+    function mergeSamples(s1, s2) {
+        var s1 = s1.aggregated ? s1 : aggregatedSingleton(s1);
+            total = s1.totalNumber + s2.number,
+
+            avg = key => (s1[key] * s1.totalNumber + s2[key] * s2.number) / total,
+            range = key => [
+                Math.min(s1[key][0], s2[key]),
+                Math.max(s1[key][1], s2[key])
+            ];
+
+        if (!s1.aggregated) s1 = aggregatedSingleton(s1);
+
+        return {
+            id: s1.id,
+            count: s1.count + 1,
+            aggregated: true,
+            date: range('date'),
+            depth: range('depth'),
+            latitude: avg('latitude'),
+            longitude: avg('longitude'),
+            length: range('length'),
+            avgLength: (s1.avgLength * s1.totalNumber + s2.length * s2.number) / total,
+            number: range('number'),
+            totalNumber: total,
+            protected: avg('protected'),
+            species: s1.species,
+            species_code: s1.species_code,
+        }
+    }
+
+    function aggregatedSingleton(s) {
+        return Object.assign({}, s, {
+            aggregated: true,
+            count: 1,
+            date: [s.date, s.date],
+            depth: [s.depth, s.depth],
+            length: [s.length, s.length],
+            avgLength: [s.length],
+            number: [s.number, s.number],
+            avgNumber: s.number,
+            totalNumber: s.number,
+        });
+    }
+
+
     function drawSamples(samples) {
-        var nodes = overlay.children().data(samples, d => d.id),
+        var samples = geoPruneSamples(samples),
+            nodes = overlay.children().data(samples, d => d.id),
             newNodes = nodes.enter().append('svg');
+
+        console.debug('Drawing %d samples', samples.length);
 
         nodes.exit().remove();
         overlay.draw();
@@ -168,7 +334,7 @@
                 .style('fill', d => d.species.color)
                 .style('stroke', '#000')
                 .style('stroke-width', 1)
-                .attr('r', d => Math.log2(2 + d.number) * 2)
+                .attr('r', d => Math.log2(2 + (d.aggregated ? d.totalNumber : d.number)) * 2)
                 .on('mouseover', tip.show)
                 .on('mouseout', tip.hide)
 
