d2a0315afedadf6d1a14f9db2fff39da236bb9f3
Ryan Cebulko <ryan@cebulko.com>
2017-12-06 16:35:15 -0500

    throttle helper function

--- public/js/util.js
+++ public/js/util.js
@@ -4,15 +4,30 @@
 // leading edge, instead of the trailing.
 function debounce(func, wait, immediate) {
     var timeout;
-    return function() {
-        var context = this, args = arguments;
-        var later = function() {
-            timeout = null;
-            if (!immediate) func.apply(context, args);
-        };
-        var callNow = immediate && !timeout;
+
+    return function () {
+        var context = this,
+            args = arguments,
+            callNow = immediate && !timeout,
+            later = function() {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+
         clearTimeout(timeout);
         timeout = setTimeout(later, wait);
         if (callNow) func.apply(context, args);
     };
 };
+
+function throttle(func, wait) {
+    var ready = true;
+
+    return function () {
+        if (ready) {
+            ready = false;
+            setTimeout(() => ready = true, wait);
+            func.apply(this, arguments);
+        }
+    };
+}
